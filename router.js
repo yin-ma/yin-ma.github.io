4b68678e5cb40e52289847afb481ad46cdfad7e5
unknown <yinma2448@gmail.com>
2024-04-29 12:38:38 +0800

    fix router

--- router.js
+++ router.js
@@ -1,46 +1,51 @@
 const routes = {
-  "404": "/src/404.html",
-  "/": "/src/home.html",
-  "/index": "/src/home.html",
-  "/index.html": "/src/home.html",
-  "/home": "/src/home.html",
-  "/about": "/src/about.html",
-  "/codesamples": "/src/codeSample.html"
+  "404": { html:"/src/404.html" },
+  "/": { html:"/src/home.html", css:"/src/home.css", js:"/src/home.js" },
+  "/index": { html:"/src/home.html", css:"/src/home.css", js:"/src/home.js" },
+  "/index.html": { html:"/src/home.html", css:"/src/home.css", js:"/src/home.js" },
+  "/home": { html:"/src/home.html", css:"/src/home.css" },
+  "/about": { html:"/src/about.html", css:"/src/about.css" },
+  "/codesamples": { html:"/src/codeSample.html", css:"/src/codeSample.css" },
 };
 
-const route = (targetPath) => {
+
+const route = (targetPath, scroll = false) => {
   if (targetPath === window.location.pathname) return;
   window.history.pushState({}, "", targetPath);
   handleLocation();
+  if (scroll) window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 
 const handleLocation = async () => {
   const path = window.location.pathname;
-  const route = routes[path] || routes["404"];
-
-  const html = await fetch(route).then((data) => data.text());
-
-  // load css first
-  let routePart = route.split("/");
-  routePart[routePart.length-1] = routePart[routePart.length-1].slice(0, -4) + "css";
-  let exist = false;
-  for (let l of document.head.querySelectorAll("link")) {
-    if (l.getAttribute("href") === routePart.join("/")) {
-      exist = true;
-      break;
-    }
-  }
-
-  if (!exist) {
-    let link = document.createElement('link');
-    link.rel = "stylesheet";
-    link.href = routePart.join("/");
-    document.head.appendChild(link);
-  }
+  const route = routes[path].html || routes["404"].html;
+  const mainPage = document.getElementById("main-page");
   
-  // and load the html
-  document.getElementById("main-page").innerHTML = html;
+  
+  // load the html
+  await fetch(route).then((data) => data.text()).then((text) => {
+    mainPage.innerHTML = text;
+
+    if (routes[path].loaded) return;
+    
+    // load css 
+    if (routes[path].css) {
+      let link = document.createElement('link');
+      link.rel = "stylesheet";
+      link.type = 'text/css';
+      link.href = routes[path].css;
+      document.head.appendChild(link);
+    }
+    
+    // load js
+    if (routes[path].js) {
+      let script = document.createElement('script');
+      script.src = routes[path].js;
+      document.head.appendChild(script);
+    }
+    routes[path].loaded = true;
+  });
 };
 
 window.onpopstate = handleLocation;
--- src/home.js
+++
@@ -1,8 +0,0 @@
-const linkBtn = document.querySelector(".home-link");
-
-linkBtn.addEventListener("hover", () => {
-  console.log("oh hi!");
-})
-
-
-console.log("hmmmmm")
