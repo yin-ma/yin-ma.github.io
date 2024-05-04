8118ee9796af55d48420e06179ddb03a3c65e0a9
unknown <yinma2448@gmail.com>
2024-05-04 19:47:00 +0800

    add pfinding algo

--- src/pathFinding/pathfinding.js
+++ src/pathFinding/pathfinding.js
@@ -1,4 +1,7 @@
-const Grid = {
+import { MinHeap } from "./libs/priorityQueue.js";
+
+
+const tile = {
   space: 0,
   wall: 1,
   start: 2,
@@ -6,12 +9,11 @@ const Grid = {
 }
 
 const gridSize = 30;
-let startRow = 3;
-let startCol = 3;
-let endRow = 5;
-let endCol = 5;
-// let endRow = gridSize-3;
-// let endCol = gridSize-3;
+let startRow = 10;
+let startCol = 10;
+let endRow = 20;
+let endCol = 20;
+
 
 let canvas = document.querySelector(".canvas");
 let algos = document.querySelector(".algos");
@@ -28,6 +30,13 @@ let removeWall = false;
 gridInit();
 
 startBtn.addEventListener("click", () => {
+  elementGrid.forEach(row => {
+    row.forEach(e => {
+      e.classList.remove("open");
+      e.classList.remove("path");
+    })
+  });
+
   switch (algos.options[algos.selectedIndex].value) {
     case "BFS":
       bfs();
@@ -36,8 +45,13 @@ startBtn.addEventListener("click", () => {
       dfs();
       break;
     case "Dijkstra":
+      dijkstra();
+      break;
+    case "GBFS":
+      gbfs();
       break;
     case "AStar":
+      astar();
       break;
     default:
       break;
@@ -48,106 +62,6 @@ clearBtn.addEventListener("click", () => {
   gridInit();
 })
 
-
-async function bfs() {
-  let openList = [[startRow, startCol]];
-  let closeList = new Set();
-  let path = {};
-  // closeList.add(`${startRow},${startCol}`);
-
-  while (openList.length > 0) {
-    let [currRow, currCol] = openList.shift();
-
-    if (closeList.has(`${currRow},${currCol}`)) continue;
-    closeList.add(`${currRow},${currCol}`);
-
-    if (currRow === endRow && currCol === endCol) break;
-
-    let direction = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    for (let [dr, dc] of direction) {
-      let nxtRow = currRow + dr;
-      let nxtCol = currCol + dc;
-      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
-      if (closeList.has(`${nxtRow},${nxtCol}`)) continue;
-      openList.push([nxtRow, nxtCol]);
-      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
-      closeList.add(`${currRow},${currCol}`);
-    }
-  }
-
-
-  for (let temp of closeList) {
-    let [tempRow, tempCol] = temp.split(",").map(e => parseInt(e));
-
-    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
-    elementGrid[tempRow][tempCol].classList.add("close");
-    await new Promise(resolve => setTimeout(resolve, 10));
-  }
-
-  let currRow = endRow;
-  let currCol = endCol;
-
-  while (true) {
-    let temp = path[`${currRow},${currCol}`].split(",");
-    currRow = parseInt(temp[0]);
-    currCol = parseInt(temp[1]);
-    if (currRow === startRow && currCol === startCol) break;
-
-    elementGrid[currRow][currCol].classList.remove("close");
-    elementGrid[currRow][currCol].classList.add("path");
-    await new Promise(resolve => setTimeout(resolve, 10));
-  }
-}
-
-
-async function dfs() {
-  let openList = [[startRow, startCol]];
-  let closeList = new Set();
-  let path = {};
-  // closeList.add(`${startRow},${startCol}`);
-
-  while (openList.length > 0) {
-    let [currRow, currCol] = openList.pop();
-
-    if (closeList.has(`${currRow},${currCol}`)) continue;
-    closeList.add(`${currRow},${currCol}`);
-
-    if (currRow === endRow && currCol === endCol) break;
-
-    let direction = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    for (let [dr, dc] of direction) {
-      let nxtRow = currRow + dr;
-      let nxtCol = currCol + dc;
-      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
-      if (closeList.has(`${nxtRow},${nxtCol}`)) continue;
-      openList.push([nxtRow, nxtCol]);
-      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
-    }
-  }
-
-
-  for (let temp of closeList) {
-    let [tempRow, tempCol] = temp.split(",").map(e => parseInt(e));
-
-    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
-    elementGrid[tempRow][tempCol].classList.add("close");
-    await new Promise(resolve => setTimeout(resolve, 10));
-  }
-
-  let currRow = endRow;
-  let currCol = endCol;
-
-  while (true) {
-    let temp = path[`${currRow},${currCol}`].split(",");
-    currRow = parseInt(temp[0]);
-    currCol = parseInt(temp[1]);
-    if (currRow === startRow && currCol === startCol) break;
-
-    elementGrid[currRow][currCol].classList.remove("close");
-    elementGrid[currRow][currCol].classList.add("path");
-    await new Promise(resolve => setTimeout(resolve, 10));
-  }
-}
 
 function gridInit() {
   canvas.innerHTML = "";
@@ -171,8 +85,8 @@ function gridInit() {
   
   elementGrid[startRow][startCol].classList.add("start");
   elementGrid[endRow][endCol].classList.add("end");
-  grid[startRow][startCol] = Grid.start;
-  grid[endRow][endCol] = Grid.end;
+  grid[startRow][startCol] = tile.start;
+  grid[endRow][endCol] = tile.end;
 }
 
 
@@ -197,12 +111,12 @@ document.addEventListener("mousedown", event =>{
   else if (event.target.classList.contains("wall")) {
     removeWall = true;
     event.target.classList.remove("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.space;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.space;
   }
   else {
     buildWall = true;
     event.target.classList.add("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.wall;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.wall;
   }
 })
 
@@ -233,22 +147,22 @@ document.addEventListener("mouseover", event => {
     if (event.target.classList.contains("end")) return;
     event.target.classList.add("start");
     event.target.classList.remove("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.start;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.start;
   }
   else if (endIsDragging === true) {
     if (event.target.classList.contains("start")) return;
     event.target.classList.add("end");
     event.target.classList.remove("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.end;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.end;
   } 
   else if (buildWall === true) {
     if (event.target.classList.contains("start") || event.target.classList.contains("end")) return ;
     event.target.classList.add("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.wall;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.wall;
   }
   else if (removeWall === true) {
     event.target.classList.remove("wall");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.space;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.space;
   }
   event.stopPropagation();
 })
@@ -259,11 +173,273 @@ document.addEventListener("mouseout" , event => {
 
   if (startIsDragging === true) {
     event.target.classList.remove("start");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.space;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.space;
   }
   else if (endIsDragging === true) {
     event.target.classList.remove("end");
-    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = Grid.space;
+    grid[parseInt(event.target.getAttribute("row"))][parseInt(event.target.getAttribute("col"))] = tile.space;
   }
   event.stopPropagation();
 })
+
+
+
+async function bfs() {
+  let openList = [[startRow, startCol]];
+  let closeList = new Set();
+  let ani = [];
+  let path = {};
+
+  while (openList.length > 0) {
+    let [currRow, currCol] = openList.shift();
+    
+    if (currRow === endRow && currCol === endCol) break;
+    if (closeList.has(`${currRow},${currCol}`)) continue;
+    closeList.add(`${currRow},${currCol}`); 
+
+    let direction = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+    for (let [dr, dc] of direction) {
+      let nxtRow = currRow + dr;
+      let nxtCol = currCol + dc;
+      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
+      if (closeList.has(`${nxtRow},${nxtCol}`)) continue;
+      if (grid[nxtRow][nxtCol] === tile.wall) continue;
+      openList.push([nxtRow, nxtCol]);
+      ani.push([nxtRow, nxtCol])
+      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
+    }
+  }
+
+  for (let temp of ani) {
+    let [tempRow, tempCol] = temp;
+
+    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
+    elementGrid[tempRow][tempCol].classList.add("open");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  let currRow = endRow;
+  let currCol = endCol;
+
+  while (true) {
+    let temp = path[`${currRow},${currCol}`].split(",");
+    currRow = parseInt(temp[0]);
+    currCol = parseInt(temp[1]);
+    if (currRow === startRow && currCol === startCol) break;
+
+    elementGrid[currRow][currCol].classList.remove("open");
+    elementGrid[currRow][currCol].classList.add("path");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
+
+async function dfs() {
+  let openList = [[startRow, startCol]];
+  let closeList = new Set();
+  let ani = [];
+  let path = {};
+
+  while (openList.length > 0) {
+    let [currRow, currCol] = openList.pop();
+    
+    if (currRow === endRow && currCol === endCol) break;
+    if (closeList.has(`${currRow},${currCol}`)) continue;
+    closeList.add(`${currRow},${currCol}`);
+
+    let direction = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+    for (let [dr, dc] of direction) {
+      let nxtRow = currRow + dr;
+      let nxtCol = currCol + dc;
+      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
+      if (closeList.has(`${nxtRow},${nxtCol}`)) continue;
+      if (grid[nxtRow][nxtCol] === tile.wall) continue;
+      openList.push([nxtRow, nxtCol]);
+      ani.push([nxtRow, nxtCol])
+      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
+    }
+  }
+
+
+  for (let temp of ani) {
+    let [tempRow, tempCol] = temp;
+
+    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
+    elementGrid[tempRow][tempCol].classList.add("open");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  let currRow = endRow;
+  let currCol = endCol;
+
+  while (true) {
+    let temp = path[`${currRow},${currCol}`].split(",");
+    currRow = parseInt(temp[0]);
+    currCol = parseInt(temp[1]);
+    if (currRow === startRow && currCol === startCol) break;
+
+    elementGrid[currRow][currCol].classList.remove("open");
+    elementGrid[currRow][currCol].classList.add("path");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
+
+async function dijkstra() {
+  let openList = [[0, startRow, startCol]];
+  let closeList = new Set();
+  let ani = [];
+  let path = {};
+
+  while (openList.length > 0) {
+    let [gCost, currRow, currCol] = MinHeap.pop(openList);
+    
+    if (currRow === endRow && currCol === endCol) break;
+    if (closeList.has(`${currRow},${currCol}`)) continue;
+    closeList.add(`${currRow},${currCol}`);
+
+    let direction = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+    for (let [dr, dc] of direction) {
+      let nxtRow = currRow + dr;
+      let nxtCol = currCol + dc;
+      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
+      if (grid[nxtRow][nxtCol] === tile.wall) continue;
+      if (closeList.has(`${nxtRow},${nxtCol}`)) continue;
+      MinHeap.push(openList, [gCost+1, nxtRow, nxtCol]);
+      ani.push([nxtRow, nxtCol]);
+      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
+    }
+  }
+
+
+  for (let temp of ani) {
+    let [tempRow, tempCol] = temp;
+
+    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
+    elementGrid[tempRow][tempCol].classList.add("open");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  let currRow = endRow;
+  let currCol = endCol;
+
+  while (true) {
+    let temp = path[`${currRow},${currCol}`].split(",");
+    currRow = parseInt(temp[0]);
+    currCol = parseInt(temp[1]);
+    if (currRow === startRow && currCol === startCol) break;
+
+    elementGrid[currRow][currCol].classList.remove("open");
+    elementGrid[currRow][currCol].classList.add("path");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
+
+async function gbfs() {
+  function heuristic(pr1, pc1, pr2, pc2) {
+    return Math.abs(pr2-pr1) + Math.abs(pc2-pc1);
+  }
+  let openList = [[heuristic(startRow, startCol, endRow, endCol), startRow, startCol]];
+  let closeList = {};
+  let ani = [];
+  let path = {};
+  path[`${startRow},${startCol}`] = `${startRow},${startCol}`;
+
+  while (openList.length > 0) {
+    let [hcost, currRow, currCol] = MinHeap.pop(openList);
+    
+    if (currRow === endRow && currCol === endCol) break;
+
+    if ((`${currRow},${currCol}` in closeList) && closeList[`${currRow},${currCol}`] >= hcost) continue;
+    closeList[`${currRow},${currCol}`] = hcost;
+    
+    let direction = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+    for (let [dr, dc] of direction) {
+      let nxtRow = currRow + dr;
+      let nxtCol = currCol + dc;
+      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
+      if (grid[nxtRow][nxtCol] === tile.wall) continue;
+      MinHeap.push(openList, [heuristic(nxtRow, nxtCol, endRow, endCol), nxtRow, nxtCol]);
+      ani.push([nxtRow, nxtCol])
+      if ((`${nxtRow},${nxtCol}` in closeList) && closeList[`${nxtRow},${nxtCol}`] >= heuristic(nxtRow, nxtCol, endRow, endCol)) continue;
+      path[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
+    }
+  }
+
+  for (let temp of ani) {
+    let [tempRow, tempCol] = temp;
+
+    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
+    elementGrid[tempRow][tempCol].classList.add("open");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  let currRow = endRow;
+  let currCol = endCol;
+
+  while (true) {
+    let temp = path[`${currRow},${currCol}`].split(",");
+    currRow = parseInt(temp[0]);
+    currCol = parseInt(temp[1]);
+    if (currRow === startRow && currCol === startCol) break;
+
+    elementGrid[currRow][currCol].classList.remove("open");
+    elementGrid[currRow][currCol].classList.add("path");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
+
+async function astar() {
+  function heuristic(pr1, pc1, pr2, pc2) {
+    return Math.abs(pr2-pr1) + Math.abs(pc2-pc1);
+  }
+  let openList = [[heuristic(startRow, startCol, endRow, endCol), 0, heuristic(startRow, startCol, endRow, endCol), startRow, startCol]];
+  let closeList = {};
+  let ani = [];
+  let pathRecord = {};
+
+  while (openList.length > 0) {
+    let [fCost, gCost, hCost, currRow, currCol] = MinHeap.pop(openList);
+    
+    if (currRow === endRow && currCol === endCol) break;
+
+    if (`${currRow},${currCol}` in closeList) continue;
+    closeList[`${currRow},${currCol}`] = gCost;
+    
+    let direction = [[0, -1], [-1, 0], [1, 0], [0, 1]];
+    for (let [dr, dc] of direction) {
+      let nxtRow = currRow + dr;
+      let nxtCol = currCol + dc;
+      if (nxtRow < 0 || nxtRow >= gridSize || nxtCol < 0 || nxtCol >= gridSize) continue;
+      if (grid[nxtRow][nxtCol] === tile.wall) continue;
+
+      MinHeap.push(openList, [gCost+heuristic(nxtRow, nxtCol, endRow, endCol)+1, gCost+1, heuristic(nxtRow, nxtCol, endRow, endCol), nxtRow, nxtCol]);
+      ani.push([nxtRow, nxtCol])
+      pathRecord[`${nxtRow},${nxtCol}`] = `${currRow},${currCol}`;
+    }
+  }
+
+  for (let temp of ani) {
+    let [tempRow, tempCol] = temp;
+
+    if ((tempRow === startRow && tempCol === startCol) || (tempRow === endRow && tempCol === endCol)) continue;
+    elementGrid[tempRow][tempCol].classList.add("open");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+
+  let currRow = endRow;
+  let currCol = endCol;
+  while (true) {
+    let temp = pathRecord[`${currRow},${currCol}`].split(",");
+    currRow = parseInt(temp[0]);
+    currCol = parseInt(temp[1]);
+    if (currRow === startRow && currCol === startCol) break;
+
+    elementGrid[currRow][currCol].classList.remove("open");
+    elementGrid[currRow][currCol].classList.add("path");
+    await new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
+
