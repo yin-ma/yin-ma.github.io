99abaea4b6b10ab644383e1ec9d1923e7a8da2ce
unknown <yinma2448@gmail.com>
2024-05-12 21:54:57 +0800

    update gitignore

--- src/aStarAlgorithm/aStarAlgorithm.js
+++ src/aStarAlgorithm/aStarAlgorithm.js
@@ -3,18 +3,23 @@ import { MinHeap } from "./libs/priorityQueue.js";
 
 let canvas = document.querySelector(".canvas");
 let numGrid = 12;
-let gridsize = canvas.getBoundingClientRect().width / numGrid;
 let grid = new Grid(canvas, numGrid, numGrid);
 
-
 grid.grid[3][3].state = grid.gridType.start;
-grid.grid[4][4].state = grid.gridType.end;
-grid.grid[5][5].state = grid.gridType.wall;
-grid.grid[6][6].state = grid.gridType.open;
-grid.grid[7][7].state = grid.gridType.close;
+grid.grid[9][9].state = grid.gridType.end;
+grid.grid[8][4].state = grid.gridType.wall;
+grid.grid[7][5].state = grid.gridType.wall;
+grid.grid[6][6].state = grid.gridType.wall;
+grid.grid[5][7].state = grid.gridType.wall;
+grid.grid[4][8].state = grid.gridType.wall;
+grid.grid[0][0].state = grid.gridType.open;
+grid.grid[0][1].state = grid.gridType.close;
 grid.draw();
 
-// function astar() {
-//   let openList = [];
-//   let closeList = [];
-// }
+astar();
+
+
+function astar() {
+  let openList = [];
+  let closeList = new Set();
+}
