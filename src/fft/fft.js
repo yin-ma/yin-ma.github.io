8f798696d0646937ffbd7d08f907fc7e1b16797c
unknown <yinma2448@gmail.com>
2024-05-16 14:12:13 +0800

    update fft

--- src/fft/fft.js
+++ src/fft/fft.js
@@ -9,29 +9,136 @@ ctx.setTransform(1,0,0,-1,0,canvas.height);
 ctx.translate(canvasWidth/2, canvasHeight/2);
 ctxFre.setTransform(1,0,0,-1,0,canvas.height);
 
-drawAxis();
-drawWave([1, 2, 3, 4, 3, 2, 1, 0]);
+let icanvas = document.querySelector(".icanvas-fre");
+let icanvasWave = document.querySelector(".icanvas-wave");
+let ictx = icanvas.getContext("2d");
+let ictxWave = icanvasWave.getContext("2d");
+ictx.setTransform(1,0,0,-1,0,canvas.height);
+ictxWave.setTransform(1,0,0,-1,0,canvas.height);
+ictxWave.translate(canvasWidth/2, canvasHeight/2);
 
-let points = [];
+let arr = new Array(512).fill(0);
+let iarr = new Array(512).fill(0);
+let isDrawing = false;
+
+init();
+
+canvas.addEventListener("mousedown", event => {
+  isDrawing = true;
+})
+
+canvas.addEventListener("mousemove", event => {
+  if (isDrawing) {
+    arr[Math.min(Math.max(0, parseInt(event.offsetX)), arr.length-1)] =  new Complex((-parseInt(Math.min(event.offsetY, canvasHeight)) + canvasHeight/2) / canvasHeight * 2, 0);
+    render(arr);
+  }
+})
+
+document.addEventListener("mouseup", event => {
+  isDrawing = false;
+})
+
+icanvas.addEventListener("mousedown", () => {
+  isDrawing = true;
+})
+
+icanvas.addEventListener("mousemove", event => {
+  if (isDrawing) {
+    iarr[parseInt(event.offsetX/2)%iarr.length] = (-parseInt(event.offsetY) + canvasHeight) / canvasHeight;
+    irender(iarr);
+  }
+})
+
+icanvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+})
+
+function irender(p) {
+  ictx.clearRect(0, 0, canvasWidth, canvasHeight);
+  ictxWave.clearRect(-canvasWidth/2, -canvasHeight/2, canvasWidth, canvasHeight);
+  drawiFrequency(p);
+  drawIwave(iFFT(p.map(v => new Complex(v, 0))));
+}
+
+function render(p) {
+  ctx.clearRect(-canvasWidth/2, -canvasHeight/2, canvasWidth, canvasHeight);
+  ctxFre.clearRect(0, 0, canvasWidth, canvasHeight);
+  drawAxis();
+  drawWave(p);
+  drawFrequency(FFT(p));
+}
+
+function init() {
+  drawAxis();
+  for (let i=0; i < arr.length; i++) {
+    let thetha = Math.PI*2/arr.length*i;
+    arr[i] = Math.cos(thetha) + Math.cos(3*thetha) + 4*Math.cos(4*thetha) + Math.cos(5*thetha);
+  }
+  let maxAmp = Math.max(...arr);
+  arr = arr.map(v => new Complex(v / maxAmp, 0));
+  let fftArr = FFT(arr);
+  drawWave(arr);
+  drawFrequency(fftArr);
+
+  iarr[1] = 0.25;
+  iarr[3] = 0.25;
+  iarr[4] = 1;
+  iarr[5] = 0.25;
+  drawiFrequency(iarr);
+  let ifftIarr = iFFT(iarr.map(v => new Complex(v*canvasHeight, 0)));
+  drawIwave(ifftIarr);
+}
+
+function drawIwave(p) {
+  let realArray = p.map(v => v.real);
+  let absArray = p.map(v => Math.abs(v.real));
+  let maxAmp = Math.max(...absArray);
+  ictxWave.lineWidth = 1;
+  ictxWave.strokeStyle = "orange";
+  ictxWave.beginPath();
+  for (let i=0; i<p.length-1; i++) {
+    ictxWave.moveTo(-canvasWidth/2 + canvasWidth / (realArray.length-1) *i, realArray[i]*(canvasHeight/2)/ maxAmp);
+    ictxWave.lineTo(-canvasWidth/2 + canvasWidth / (realArray.length-1) *(i+1), realArray[i+1] *(canvasHeight/2)/ maxAmp);
+  }
+  ictxWave.stroke();
+}
+
+function drawiFrequency(p) {
+  ictx.lineWidth = 2;
+  for (let i=0; i<p.length/2; i++) {
+    ictx.strokeStyle = `hsl(${i / canvasWidth *360}, 76%, 50%)`;
+    ictx.beginPath();
+    ictx.moveTo(i*2, 0);
+    ictx.lineTo(i*2, p[i]*canvasHeight);
+    ictx.stroke();
+  }
+}
+
+
+function drawFrequency(p) {
+  let absArray = p.map(v => abs(v))
+  let maxAmp = Math.max(...absArray);
+  ctxFre.lineWidth = 2;
+  for (let i=0; i<absArray.length/2; i++) {
+    ctxFre.strokeStyle = `hsl(${i / canvasWidth *360}, 76%, 50%)`;
+    ctxFre.beginPath();
+    ctxFre.moveTo(canvasWidth / absArray.length * i * 2, 0);
+    ctxFre.lineTo(canvasWidth / absArray.length * i * 2, absArray[i] / maxAmp * canvasHeight)
+    ctxFre.stroke();
+  }
+}
 
 function drawWave(p) {
-  let maxAmp = Math.max(Math.abs(Math.max(...p)), Math.abs(Math.min(...p)));
-  ctx.lineWidth = 3;
+  let realArray = p.map(v => v.real);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "orange";
   ctx.beginPath();
   for (let i=0; i<p.length-1; i++) {
-    ctx.moveTo(-canvasWidth/2 + canvasWidth / (p.length-1) *i, p[i]/maxAmp*(canvasHeight/2));
-    ctx.lineTo(-canvasWidth/2 + canvasWidth / (p.length-1) *(i+1), p[i+1] / maxAmp*(canvasHeight/2));
+    ctx.moveTo(-canvasWidth/2 + canvasWidth / (realArray.length-1) *i, realArray[i]*(canvasHeight/2));
+    ctx.lineTo(-canvasWidth/2 + canvasWidth / (realArray.length-1) *(i+1), realArray[i+1] *(canvasHeight/2));
   }
   ctx.stroke();
 }
-
-// ctx.beginPath();
-// for (let i=0; i<parseInt(canvasWidth); i++) {
-//   ctx.moveTo(-canvasWidth/2 + i, canvasHeight/2*Math.sin(Math.PI*2/canvasWidth*i));
-//   ctx.lineTo(-canvasWidth/2 + i+1, canvasHeight/2*Math.sin(Math.PI*2/canvasWidth*(i+1)));
-//   ctx.stroke();
-//   points.push([-canvasWidth/2 + i, canvasHeight/2*Math.sin(Math.PI*2/canvasWidth*i)]);
-// }
 
 function drawAxis() {
   ctx.beginPath();
@@ -42,7 +149,3 @@ function drawAxis() {
   ctx.stroke();
   ctx.setLineDash([]);
 }
-
-
-
-// FFT([new Complex(3, 0), new Complex(7, 0), new Complex(21, 0), new Complex(5, 0)]);
--- src/fft/utils.js
+++ src/fft/utils.js
@@ -38,6 +38,42 @@ function FFT(P) {
   return y;
 }
 
+function iFFT(P) {
+  function helper(P) {
+    let n = P.length;
+  
+    if (n & (n-1) !== 0 || n === 0) {
+      throw "array length should be power of 2";
+    }
+      
+    if (n === 1)
+      return P;
+  
+    let omega = [];
+    for (let i=0; i<n; i++) {
+      omega.push(new Complex(Math.cos(Math.PI*2*i/n), Math.sin(Math.PI*2*i/n)));
+    }
+  
+    let Pe = P.filter((v, i) => (i % 2) === 0 );
+    let Po = P.filter((v, i) => (i % 2) === 1 );
+  
+    let ye = helper(Pe);
+    let yo = helper(Po);
+    let y = [];
+  
+    for (let i=0; i<n; i++) {
+      y.push(0);
+    }
+  
+    for (let i=0; i<n/2; i++) {
+      y[i] = add(ye[i], multiply(omega[i], yo[i]));
+      y[i+n/2] = subtract(ye[i], multiply(omega[i], yo[i]));
+    }
+    return y;
+  }
+  return helper(P).map(v => new Complex(v.real/P.length, v.img/P.length));
+}
+
 function add(a, b) {
   return new Complex(a.real + b.real, a.img + b.img);
 }
@@ -58,4 +94,12 @@ function abs(a) {
 
 function conjugate(a) {
   return new Complex(a.real, -a.img);
+}
+
+function dot(a, b) {
+  let sum = 0;
+  for (let i=0; i<a.length; i++) {
+    sum += a.real * b.real + a.img * b.img;
+  }
+  return sum
 }
