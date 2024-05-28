e9f3dba3e2c3826f87b168ed21fc97648a9c4161
unknown <yinma2448@gmail.com>
2024-05-28 22:14:31 +0800

    update minimax

--- src/minimax/minimax.js
+++ src/minimax/minimax.js
@@ -1,19 +1,20 @@
 let board = document.querySelector(".gameboard");
 let numGrid = 7;
+let maxDepth = 6;
 let gridSize = board.getClientRects()[0].width / numGrid;
 let html = document.querySelector("html").style.setProperty("--grid-size", `${gridSize}px`);
 let game = new ConnectFour(board, numGrid, gridSize);
-let agent = new MinimaxAgent();
-agent.maxDepth = 6;
+let agent = new MinimaxAgent(2, maxDepth);
 
 board.addEventListener("click", async event => {
-  await game.move(event.target.getAttribute("col"));
+  if (game.turn === 1 && !game.isWin) {
+    await game.move(event.target.getAttribute("col"));
+  }
 
   if (game.turn === 2 && !game.isWin) {
     let temp = agent.copyBoard(game.board);
-    agent.bestMove = null;
-    agent.minimax(temp, 6, true);
-    await game.move(agent.bestMove);
+    agent.setPlayer(2);
+    await game.move(agent.getBestMove(temp));
   }
 
 })
--- src/minimax/minimaxAgent.js
+++ src/minimax/minimaxAgent.js
@@ -1,18 +1,36 @@
 class MinimaxAgent {
-  constructor() {
+  constructor(player=2, maxDepth=4) {
     this.bestMove = null;
-    this.maxDepth = 4;
+    this.player = player;
+    this.maxDepth = maxDepth;
+  }
+  
+  setPlayer(player) {
+    this.player = player;
+  }
+
+  setMaxDepth(d) {
+    this.maxDepth = d;
+  }
+
+  getBestMove(board) {
+    this.bestMove = null;
+
+    this.minimax(board, this.maxDepth, true);
+
+    return this.bestMove;
   }
 
   minimax(node, depth, maximizingPlayer) {
     if (this.isTerminal(node)) {
       if (maximizingPlayer) {
-        return -1000000;
+        return -10000;
       } else{
         return 1000;
       }
-    } else if (depth === 0) {
-      return this.heurisic(node, maximizingPlayer);
+    } 
+    if (depth === 0) {
+      return this.heurisic(node);
     }
 
     if (maximizingPlayer) {
@@ -20,10 +38,10 @@ class MinimaxAgent {
       for (let c=0; c<node[0].length; c++) {
         if (!this.isMoveValid(node, c)) continue;
         let temp = this.copyBoard(node);
-        this.move(temp, c, 2);
-        let newV = this.minimax(temp, depth-1, false);
-        if (newV > value) {
-          value = newV;
+        this.move(temp, c, this.player);
+        let newValue = this.minimax(temp, depth-1, false);
+        if (newValue > value) {
+          value = newValue;
           if (depth === this.maxDepth) {
             this.bestMove = c;
           }
@@ -37,7 +55,8 @@ class MinimaxAgent {
       for (let c=0; c<node[0].length; c++) {
         if (!this.isMoveValid(node, c)) continue;
         let temp = this.copyBoard(node);
-        this.move(temp, c, 1);
+        let opponent = (this.player === 1) ? 2: 1;
+        this.move(temp, c, opponent);
         value = Math.min(value, this.minimax(temp, depth-1, true));
       }
       return value;
@@ -55,13 +74,13 @@ class MinimaxAgent {
   countConnectFour(state, player)  {
     let count = 0;
     let numGrid = state[0].length;
-    let opponent = (player === 1) ? 2 : 1;
+    let opponent = (player === 2) ? 1: 2;
 
     // check verticle "|"
     for (let col=0; col<numGrid; col ++) {
       for (let row=0; row<numGrid-1-3; row++) {
-        if (state[row][col] === opponent) continue;
         if (
+          (state[row][col] === opponent) ||
           (state[row+1][col] === opponent) ||
           (state[row+2][col] === opponent) ||
           (state[row+3][col] === opponent)
@@ -75,14 +94,14 @@ class MinimaxAgent {
     // check horizontal "-"
     for (let row=0; row<numGrid-1; row++) {
       for (let col=0; col<numGrid-3; col++) {
-        if (state[row][col] === opponent) continue;
         if (
+          (state[row][col] === opponent) ||
           (state[row][col+1] === opponent) ||
           (state[row][col+2] === opponent) ||
           (state[row][col+3] === opponent)
         ) {
           continue;
-        }        
+        }   
         count += 1;
       }
     }    
@@ -90,14 +109,14 @@ class MinimaxAgent {
     // check diagonal "/"
     for (let row=3; row<numGrid-1; row++) {
       for (let col=0; col<numGrid-3; col++) {
-        if (state[row][col] === opponent) continue;
         if (
+          (state[row][col] === opponent) ||
           (state[row-1][col+1] === opponent) ||
           (state[row-2][col+2] === opponent) ||
           (state[row-3][col+3] === opponent)
         ) {
           continue;
-        }        
+        }
         count += 1;
       }
     }        
@@ -105,14 +124,14 @@ class MinimaxAgent {
     // check diagonal "\"
     for (let row=0; row<numGrid-1-3; row++) {
       for (let col=0; col<numGrid-3; col++) {
-        if (state[row][col] === opponent) continue;
         if (
+          (state[row][col] === opponent) ||
           (state[row+1][col+1] === opponent) ||
           (state[row+2][col+2] === opponent) ||
           (state[row+3][col+3] === opponent)
         ) {
           continue;
-        }        
+        }
         count += 1;
       }
     }
@@ -120,12 +139,14 @@ class MinimaxAgent {
     return count;
   }
 
-  heurisic(state, maximizingPlayer) {
+  heurisic(state) {
     // count how many connect four left
     let countYellow = this.countConnectFour(state, 2);
     let countRed = this.countConnectFour(state, 1);
 
-    if (maximizingPlayer) return (countYellow-countRed);
+    if (this.player === 2) {
+      return (countYellow-countRed);
+    }
     return -(countYellow-countRed);
   }
 
@@ -195,7 +216,7 @@ class MinimaxAgent {
   }
 
   boardIsFull(state) {
-    let numGrid = state.length;
+    let numGrid = state[0].length;
     for (let r=0; r<numGrid-1; r++) {
       for (let c=0; c<numGrid; c++) {
         if (state[r][c] === 0) {
@@ -206,11 +227,11 @@ class MinimaxAgent {
     return true;
   }
 
-  move(board, c, turn) {
-    let numGrid = board.length;
-    for (let r=numGrid-1; r>=0; r--) {
+  move(board, c, piece) {
+    let numGrid = board[0].length;
+    for (let r=numGrid-2; r>=0; r--) {
       if (board[r][c] === 0) {
-        board[r][c] = turn;
+        board[r][c] = piece;
         break;
       }
     }
